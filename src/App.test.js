bd317b0248745bedd114c6d13aa7c3938379fadb
Gary J <github@accu.ca>
2022-01-20 15:35:34 -0800

    Update

--- src/App.test.js
+++ src/App.test.js
@@ -10,53 +10,53 @@ describe("App", () => {
   it.skip('renders the header properly', () => {
     const { container } = render(<App />);
     console.log(prettyDOM(container));
+    
     const text = screen.getByText("Hello React");
     expect(text).toBeInTheDocument();
   });
 
-  it.skip('finds the Counter', () => {
-    const { container } = render(<App />);
-    console.log(prettyDOM(container));
+  it('finds the Counter', () => {
+    render(<App />);
+    // console.log(prettyDOM(container));
 
     const counter = screen.getByTestId("counter");
-    // expect(text).toBeInTheDocument();
+
+    // This is not really needed
+    expect(counter).toBeTruthy();
   });
 
-  it.skip('finds the Counter + button', () => {
-    const { container } = render(<App />);
+  it('finds the Counter + button', () => {
+    render(<App />);
 
+    // Find + button inside counter
     const counter = screen.getByTestId("counter");
-    const button = screen.getByText("+");
+    const button = within(counter).getByText("+");
     fireEvent.click(button);
 
     const count = screen.getByTestId("count");
 
     expect(count.textContent).toBe("1");
-
-    // expect(text).toBeInTheDocument();
   });
 
-  it.skip('check if list empty', () => {
+  it('check if list empty', () => {
     render(<App />);
 
     const users = screen.getByRole("list");
 
-    const list = screen.queryAllByTestId("user");
+    const list = within(users).queryAllByTestId("user");
     expect(list).toBeTruthy();
     expect(list.length).toEqual(0);
-
-    // expect(text).toBeInTheDocument();
   });
 
-  it.skip('clicks Load and checks if list has 4 items', () => {
+  it('clicks Load and checks if list has 4 items', () => {
     render(<App />);
 
     const button = screen.getByText("Load Users");
-
     fireEvent.click(button);
 
-    // // const user = screen.getByTestId("users");
+    // Get <li>'s by testid
     const list = screen.queryAllByTestId("user");
+
     expect(list).toBeTruthy();
     expect(list.length).toEqual(4);
   });
