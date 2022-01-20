e8bbaabe58f7652e9ce302ccbf72319c54a39e47
Gary J <github@accu.ca>
2022-01-20 09:15:28 -0800

    Add

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -2,13 +2,13 @@ import { render, screen } from '@testing-library/react';
 import Header from './Header';
 
 test('renders header and shows text', () => {
-  render(<Header text="learn react"/>);
+  render(<Header text="learn react" />);
   const heading = screen.getByText(/learn react/i);
   expect(heading).toBeInTheDocument();
 });
 
 test('renders header and finds heading with text, by role', () => {
-  render(<Header text="learn react"/>);
+  render(<Header text="learn react" />);
   const heading = screen.getByRole("heading");
-  expect(heading).toBeInTheDocument();
+  expect(heading.textContent).toBe("learn react");
 });
